f3f23e2968ac9a7cb804eaed9339a4e862bdff1f
Ntangda <yanngaudin64@gmail.com>
2026-04-20 11:08:31 +0200

    test done

--- tests/Unit/SaveurTest.php
+++ tests/Unit/SaveurTest.php
@@ -8,7 +8,13 @@ class SaveurTest extends TestCase
 {
      public function testIdentifiant(): void
      {
+                $saveur1 = new Saveur("Chocolat", true);
+                $saveur2 = new Saveur("Vanille", true);
+                $saveur1->SetIdentifiant("S1");
+                $saveur2->SetIdentifiant("S2");
 
+                $this->assertIsString($saveur1->getIdentifiant());
+                $this->assertIsString($saveur2->getIdentifiant());
      }
 
 
