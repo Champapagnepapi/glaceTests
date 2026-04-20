25185cd397cb379addfa041647dab2edc7c4ff4b
Ntangda <yanngaudin64@gmail.com>
2026-04-20 11:02:58 +0200

    test sur l'identifiant de saveur

---
+++ tests/Unit/SaveurTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\Saveur;
+
+class SaveurTest extends TestCase
+{
+     public function testIdentifiant(): void
+     {
+
+     }
+
+
+}
