41cc6b8c8c6af89687698efd6460ac84a9b27894
Ntangda <yanngaudin64@gmail.com>
2026-04-22 00:41:07 +0200

    tests done

--- tests/Integration/GestionCaisseIntegrationTest.php
+++ tests/Integration/GestionCaisseIntegrationTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use App\Caisse;
 use App\Glace;
 use App\Saveur;
+use App\Commande;
+use App\Stock;
+use App\Client;
 
 use PHPUnit\Framework\TestCase;
 
@@ -42,4 +45,38 @@ class GestionCaisseIntegrationTest extends TestCase
 
         $this->assertSame(125, $caisse->getSolde());
     }
+
+    public function testCaisseDecaissementInsuffisant(): void
+    {
+        $caisse = new Caisse();
+
+        $caisse->encaisser(50);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("Solde insuffisant pour ce décaissement.");
+
+        $caisse->decaisser(100);
+    }
+
+    public function testCycleVenteAchatCaisse(): void
+    {
+        $stock = new Stock();
+        $caisse = new Caisse();
+
+        $glace = $this->creerGlace("Chocolat", 10);
+        $stock->ajouter($glace, 5);
+
+
+        for ($i = 0; $i < 3; $i++) {
+            $this->assertTrue($stock->estDisponible($glace));
+            $commande = new Commande();
+            $commande->ajouterGlace($glace);
+            $stock->retirer($glace, 1);
+            $caisse->encaisser($commande->getTotal());
+        }
+
+        $this->assertSame(30, $caisse->getSolde());
+        $this->assertSame(2, $stock->getQuantite($glace));
+    }
+
 }
