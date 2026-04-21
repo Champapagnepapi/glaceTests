b4dcf338faec366436071ea361544084f30837cd
Ntangda <yanngaudin64@gmail.com>
2026-04-21 23:59:35 +0200

    premier test done

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -6,6 +6,10 @@ class StockTest extends TestCase
 {
     private function creerGlace(string $nomSaveur): Glace
     {
-      
+        $saveur = new Saveur($nomSaveur);
+        $glace = new Glace($nomSaveur, $saveur);
+        $glace->SetIdentifiant("G_" . $nomSaveur);
+
+        return $glace;
     }
 }
