a36a7824886adcba1f661a286b2cd9b54df69ec9
Ntangda <yanngaudin64@gmail.com>
2026-04-22 00:21:58 +0200

    test done creer la une glace

--- tests/Integration/GestionCaisseIntegrationTest.php
+++ tests/Integration/GestionCaisseIntegrationTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Glace;
+use App\Saveur;
 
 use PHPUnit\Framework\TestCase;
 
@@ -9,6 +11,11 @@ class GestionCaisseIntegrationTest extends TestCase
 {
     private function creerGlace(string $nomSaveur, int $prixVente): Glace
     {
+        $saveur = new Saveur($nomSaveur);
+        $glace = new Glace($nomSaveur, $saveur);
+        $glace->SetIdentifiant("G_" . $nomSaveur);
+        $glace->SetPrixVente($prixVente);
 
+        return $glace;
     }
 }
