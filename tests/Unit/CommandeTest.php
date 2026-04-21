96830e2f0781bfdb5837d1684ced2f3025cea500
Ntangda <yanngaudin64@gmail.com>
2026-04-21 23:19:52 +0200

    tous les autres tests de la classe done

--- tests/Unit/CommandeTest.php
+++ tests/Unit/CommandeTest.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Commande;
+use App\Glace;
+use App\Saveur;
 use PHPUnit\Framework\TestCase;
 
 class CommandeTest extends TestCase
@@ -15,5 +18,29 @@ class CommandeTest extends TestCase
         return $glace;
     }
 
-   
+    public function testCommandeVideATotalZero(): void
+    {
+        $commande = new Commande();
+
+        $this->assertSame(0, $commande->getTotal());
+    }
+
+    public function testAjouterGlaceAugmenteLeNombre(): void
+    {
+        $commande = new Commande();
+
+        $commande->ajouterGlace($this->creerGlace('Vanille', 5));
+
+        $this->assertSame(1, $commande->getNombreGlaces());
+    }
+
+    public function testTotalCommandeAdditionneLesPrixDeVente(): void
+    {
+        $commande = new Commande();
+
+        $commande->ajouterGlace($this->creerGlace('Chocolat', 4));
+        $commande->ajouterGlace($this->creerGlace('Fraise', 6));
+
+        $this->assertSame(10, $commande->getTotal());
+    }
 }
