38ac946bd4391a94a4ac2dcc6ef4441a3e7abaad
Ntangda <yanngaudin64@gmail.com>
2026-04-21 23:37:39 +0200

    test done

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -2,14 +2,19 @@
 
 declare(strict_types=1);
 
-
+use App\Glace;
+use App\Saveur;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
     private function creerGlace(string $nomSaveur, int $prixVente): Glace
     {
-      
+        $saveur = new Saveur($nomSaveur);
+        $glace = new Glace($nomSaveur, $saveur);
+        $glace->SetPrixVente($prixVente);
+
+        return $glace;
     }
 
     
