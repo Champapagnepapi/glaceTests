35e3c4d17504fd981a35d30eb0a4ca3674cdf41e
Ntangda <yanngaudin64@gmail.com>
2026-04-22 00:17:43 +0200

    etat initial des tests sur la caisse

---
+++ tests/Integration/GestionCaisseIntegrationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+
+use PHPUnit\Framework\TestCase;
+
+class GestionCaisseIntegrationTest extends TestCase
+{
+    private function creerGlace(string $nomSaveur, int $prixVente): Glace
+    {
+
+    }
+}
