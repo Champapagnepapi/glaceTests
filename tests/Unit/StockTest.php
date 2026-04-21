45ee0b254ca52e8fce9db2f4803a989bc6a9a4db
Ntangda <yanngaudin64@gmail.com>
2026-04-22 00:06:15 +0200

    test done tests sur le stock

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Glace;
+use App\Saveur;
+use App\Stock;
+use PHPUnit\Framework\TestCase;
+
 class StockTest extends TestCase
 {
     private function creerGlace(string $nomSaveur): Glace
@@ -11,5 +16,57 @@ class StockTest extends TestCase
         $glace->SetIdentifiant("G_" . $nomSaveur);
 
         return $glace;
+    }
+
+    public function testStockVideNePasDisponible(): void
+    {
+        $stock = new Stock();
+        $glace = $this->creerGlace("Vanille");
+
+        $this->assertFalse($stock->estDisponible($glace));
+    }
+
+    public function testAjouterGlaceAuStock(): void
+    {
+        $stock = new Stock();
+        $glace = $this->creerGlace("Chocolat");
+
+        $stock->ajouter($glace, 5);
+
+        $this->assertSame(5, $stock->getQuantite($glace));
+    }
+
+    public function testRetirerGlaceAuStock(): void
+    {
+        $stock = new Stock();
+        $glace = $this->creerGlace("Fraise");
+
+        $stock->ajouter($glace, 10);
+        $stock->retirer($glace, 3);
+
+        $this->assertSame(7, $stock->getQuantite($glace));
+    }
+
+    public function testEstDisponibleSiQuantitePositive(): void
+    {
+        $stock = new Stock();
+        $glace = $this->creerGlace("Pistache");
+
+        $stock->ajouter($glace, 2);
+
+        $this->assertTrue($stock->estDisponible($glace));
+    }
+
+    public function testRetirerPlusQueStockInterdit(): void
+    {
+        $stock = new Stock();
+        $glace = $this->creerGlace("Menthe");
+
+        $stock->ajouter($glace, 2);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("Stock insuffisant pour cette glace.");
+
+        $stock->retirer($glace, 3);
     }
 }
